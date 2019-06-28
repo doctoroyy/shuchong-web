283c627469b929009edf894e5551d43b1d46b195
doctoroyy <doctor.oyy@gmail.com>
2019-06-28 14:40:56 +0800

    修改Vue文件引用路径

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
-import Home from '../components/Home'
-import BookInfo from '../components/BookInfo'
-import SignUp from '../components/SignUp'
+import Home from '@/components/Home'
+import BookInfo from '@/components/BookInfo'
+import SignUp from '@/components/SignUp'
 
 
 Vue.use(Router)
