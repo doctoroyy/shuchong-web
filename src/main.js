720ddd8fd79314b080732d7e304e79eac9032c78
doctoroyy <doctor.oyy@gmail.com>
2019-07-03 19:10:09 +0800

    element-ui改为按需引入，缩小打包体积

--- src/main.js
+++ src/main.js
@@ -4,14 +4,84 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import {
+  Pagination,
+  Dialog,
+ 
+  Menu,
+  MenuItem,
+  Input,
+  Button,
+  Form,
+  FormItem,
+  Tag,
+  Icon,
+  Row,
+  Col,
+  
+  
+  Card,
+  
+  Carousel,
+  CarouselItem,
+ 
+  
+  Container,
+  Header,
+  Aside,
+  Main,
+  Footer,
+  
+  Divider,
+  Image,
+  
+} from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios'
 
 Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
-Vue.use(ElementUI);
+
+
+
+
+Vue.use(Button);
+
+Vue.use(Form);
+Vue.use(FormItem);
+
+Vue.use(Tag);
+
+Vue.use(Icon);
+Vue.use(Row);
+Vue.use(Col);
+
+// Vue.use(Spinner);
+
+Vue.use(Carousel);
+Vue.use(CarouselItem);
+// Vue.use(Collapse);
+// Vue.use(CollapseItem);
+// Vue.use(Cascader);
+
+Vue.use(Container);
+Vue.use(Header);
+Vue.use(Aside);
+Vue.use(Main);
+Vue.use(Footer);
+
+Vue.use(Divider);
+Vue.use(Image);
+Vue.use(Card);
+Vue.use(Input);
+Vue.use(Dialog);
+Vue.use(Menu);
+Vue.use(MenuItem);
+
+Vue.use(Pagination);
+
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
